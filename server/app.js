738762db36b54921043cb6ec2a6f116c712712ee
ruanwen <ruanwen@bytedance.com>
2022-10-21 17:26:00 +0800

    feat: 接入mongodb

--- server/app.js
+++ server/app.js
@@ -1,49 +1,3 @@
-// const Koa = require('koa')
-// const app = new Koa()
-// const views = require('koa-views')
-// const json = require('koa-json')
-// const onerror = require('koa-onerror')
-// const bodyparser = require('koa-bodyparser')
-// const logger = require('koa-logger')
-
-// const index = require('./routes/index')
-// const users = require('./routes/users')
-
-// // error handler
-// onerror(app)
-
-// // middlewares
-// app.use(bodyparser({
-//   enableTypes:['json', 'form', 'text']
-// }))
-// app.use(json())
-// app.use(logger())
-// app.use(require('koa-static')(__dirname + '/public'))
-
-// app.use(views(__dirname + '/views', {
-//   extension: 'pug'
-// }))
-
-// // logger
-// app.use(async (ctx, next) => {
-//   const start = new Date()
-//   await next()
-//   const ms = new Date() - start
-//   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-// })
-
-// // routes
-// app.use(index.routes(), index.allowedMethods())
-// app.use(users.routes(), users.allowedMethods())
-
-// // error-handling
-// app.on('error', (err, ctx) => {
-//   console.error('server error', err, ctx)
-// });
-
-// module.exports = app
-
-// 导入koa2
 const Koa = require("koa");
 const bodyParser = require('koa-bodyparser');
 const router = require('./routes/index');
@@ -51,13 +5,6 @@ const mongoose = require('mongoose');
 
 // 创建一个Koa对象表示web app本身:
 const app = new Koa();
-
-// 对于任何请求，app将调用该异步函数处理请求：
-// app.use(async (ctx, next) => {
-//   await next();
-//   ctx.response.type ='text/html';
-//   ctx.response.body ="<h1>Hello, koa2!</h1>";
-// });
 
 const MONGODBURL = 'mongodb+srv://AlbertBird:qwerasdf@cluster0.wttcowf.mongodb.net/test';
 mongoose.connect(MONGODBURL);
@@ -73,8 +20,22 @@ database.once('connected', () => {
 
 app.use(bodyParser());
 
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+});
+
 app
   .use(router.routes())  //作用：启动路由
   .use(router.allowedMethods());
+
+// error-handling
+app.on('error', (err, ctx) => {
+  console.error('server error', err, ctx)
+});
+
 // 在端口3001监听:
 app.listen(3001);
