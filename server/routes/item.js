f28b985f39dc7b34e8f4c34d1bb27f49b86d50b8
AlbertBird <1533085007@qq.com>
2022-10-28 11:54:30 +0800

    fix: 增加商品评论接口

--- server/routes/item.js
+++ server/routes/item.js
@@ -1,25 +1,20 @@
 const Router = require('koa-router');
 const connection = require('../models/db');
+const ErrorType = require('../common/errorCode');
 const router = new Router();
 
 // 商品发布
 router.post('/publish', async (ctx, next) => {
   const body = ctx.request.body;
-  const { title, type, condition, price, stock, shipment, desciption, cover } = body;
-  console.log('body', body);
+  const { title, price, type, condition, stock, shipment, desciption, cover } = body;
   const { email } = ctx.session;
   if (!email) {
-    ctx.body = {
-        code: 3000,
-        message: '请登录'
-    };
+    ctx.body = ErrorType.USERNOTLOGIN;
     next();
-}
+  }
   try {
-    const addSql = `INSERT INTO item_info  (title, type, condition, price, stock, shipment, desciption, cover, email) VALUES ('${title}', ${type}, ${condition}, ${price}, ${stock}, ${shipment}, '${desciption}', '${cover}', '${email}');`;
-    console.log('addSql', addSql);
+    const addSql = `INSERT INTO item_info (title, price, item_type, item_condition, stock, shipment, desciption, cover, email, time) VALUES ('${title}', ${price}, ${type}, ${condition}, ${stock}, ${shipment}, '${desciption}', '${cover}', '${email}', NOW());`;
     const res = await connection.query(addSql);
-    console.log('addSql', res);
     ctx.body = {
       code: 0,
       message: '发布成功'
@@ -35,44 +30,52 @@ router.post('/publish', async (ctx, next) => {
 router.post('/update', async (ctx, next) => {
   const body = ctx.request.body;
   const { id, title, type, condition, price, stock, shipment, desciption, cover } = body;
-  console.log('body', body);
   const { email } = ctx.session;
   if (!email) {
-    ctx.body = {
-        code: 3000,
-        message: '请登录'
-    };
+    ctx.body = ErrorType.USERNOTLOGIN;
     next();
   }
   try {
-    const querySql = `SELECT * FROM item_info WHERE id=${id}`;
+    const querySql = `SELECT * FROM item_info WHERE item_id=${id}`;
     const [res] = await connection.query(querySql);
     console.log('res1', res);
     if (!res?.length) {
-      ctx.body = {
-        code: 2002,
-        message: '该商品不存在'
-      };
+      ctx.body = ErrorType.ITEMNOTEXIST;
     } else {
       // 这块看看怎么做增量更新
-      let updateSql = 'Update user_info_test SET ';
+      let updateSql = `Update item_info SET `;
       // TODO: sql语句有点问题
-      if (username) {
-        updateSql += `user_name='${username}', `;
+      if (title) {
+        updateSql += `title='${title}', `;
       }
-      if (phone) {
-        updateSql += `phone='${phone}', `;
+      if (price) {
+        updateSql += `price='${price}', `;
       }
-      if (facebookAccount) {
-        facebook_account += `user_name='${facebookAccount}', `;
+      if (type) {
+        updateSql += `item_type='${type}', `;
       }
-      updateSql += ` WHERE user_email='${email}'`;
-      // const updateSql = `Update user_info_test SET user_name='${username}', phone='${phone}', facebook_account='${facebookAccount}' WHERE user_email='${email}'`;
+      if (condition) {
+        updateSql += `item_condition='${condition}', `;
+      }
+      if (stock) {
+        updateSql += `stock='${stock}', `;
+      }
+      if (shipment) {
+        updateSql += `shipment='${shipment}', `;
+      }
+      if (desciption) {
+        updateSql += `desciption='${desciption}', `;
+      }
+      if (cover) {
+        updateSql += `cover='${cover}', `;
+      }
+      updateSql += `time=now(), `;
+      updateSql = updateSql.slice(0 , -2);
+      updateSql += ` WHERE item_id=${id}`;
       const res = await connection.query(updateSql);
-      console.log('addSql', res);
       ctx.body = {
         code: 0,
-        message: '修改信息成功'
+        message: '修改成功'
       }
     }
   } catch (error) {
@@ -89,23 +92,17 @@ router.post('/delete', async (ctx, next) => {
   console.log('body', body);
   const { email } = ctx.session;
   if (!email) {
-    ctx.body = {
-        code: 3000,
-        message: '请登录'
-    };
+    ctx.body = ErrorType.USERNOTLOGIN;
     next();
   }
   try {
-    const querySql = `SELECT * FROM item_info WHERE id=${id}`;
+    const querySql = `SELECT * FROM item_info WHERE item_id=${id}`;
     const [res] = await connection.query(querySql);
     console.log('res1', res);
     if (!res?.length) {
-      ctx.body = {
-        code: 2002,
-        message: '该商品不存在'
-      };
+      ctx.body = ErrorType.ITEMNOTEXIST;
     } else {
-      const deleteSql = `DELETE FROM item_info WHERE id=${id}`;
+      const deleteSql = `DELETE FROM item_info WHERE item_id=${id}`;
       const res = await connection.query(deleteSql);
       ctx.body = {
         code: 0,
@@ -121,17 +118,15 @@ router.post('/delete', async (ctx, next) => {
 
 // 获取商品信息
 router.post('/info', async (ctx, next) => {
-  const { id } = ctx.request.body;
+  const body = ctx.request.body;
   console.log('body', body);
+  const { id } = body;
   try {
-    const querySql = `SELECT * FROM item_info WHERE id=${id}`;
+    const querySql = `SELECT * FROM item_info WHERE item_id=${id}`;
     const [res] = await connection.query(querySql);
     console.log('res1', res);
     if (!res?.length) {
-      ctx.body = {
-        code: 2002,
-        message: '商品不存在',
-      };
+      ctx.body = ErrorType.ITEMNOTEXIST;
     } else {
       const data = res[0];
       const { email } = data;
@@ -160,7 +155,6 @@ router.get('/list', async (ctx, next) => {
   try {
     const querySql = 'SELECT * FROM item_info';
     const [res] = await connection.query(querySql);
-    console.log('res1', res);
     ctx.body = {
       data: res,
       code: 0,
@@ -175,32 +169,74 @@ router.get('/list', async (ctx, next) => {
 
 // 卖家商店信息
 router.post('/seller', async (ctx, next) => {
-  const { id } = ctx.request.body;
-  console.log('body', body);
+  const { userId } = ctx.request.body;
   try {
-    const querySql = `SELECT * FROM user_info_test WHERE id=${id}`;
+    const querySql = `SELECT * FROM user_info_test WHERE user_id=${userId}`;
     const [res] = await connection.query(querySql);
-    console.log('res1', res);
     if (!res?.length) {
-      ctx.body = {
-        code: 2002,
-        message: '用户不存在'
-      };
+      ctx.body = ErrorType.USERNOTEXIST;
     } else {
-      const { email } = res[0];
-      const querySql = `SELECT * FROM item_info WHERE email='${email}'`;
+      const { user_email } = res[0];
+      const querySql = `SELECT * FROM item_info WHERE email='${user_email}'`;
       const [list] = await connection.query(querySql);
       console.log('addSql', res);
       ctx.body = {
         data: {
           sellerInfo: res[0],
-          list: list[0]
+          list: list
         },
         code: 0,
         message: 'success'
       }
     }
   } catch (error) {
+    ctx.body = {
+      message: error.message
+    };
+  }
+})
+
+// 商品评论更新
+router.post('/updateComment', async (ctx, next) => {
+  const body = ctx.request.body;
+  const { id, comment } = body;
+  const { email } = ctx.session;
+  if (!email) {
+    ctx.body = ErrorType.USERNOTLOGIN;
+    next();
+  }
+  try {
+    const querySql = `SELECT * FROM item_info WHERE item_id=${id}`;
+    const [res] = await connection.query(querySql);
+    console.log('res11111', res, !res?.length);
+    if (!res?.length) {
+      console.log('item111', 111);
+      ctx.body = ErrorType.ITEMNOTEXIST;
+    } else {
+      console.log('item222', 222);
+      const { comments } = res[0];
+      let commentsData = [];
+
+      try {
+        commentsData = comments ? JSON.parse(comments) : [];
+      } catch {
+      }
+      commentsData.push({
+        content: comment,
+        name: email,
+        time: Date.now()
+      });
+      // 这块看看怎么做增量更新
+      let updateSql = `Update item_info SET comments='${JSON.stringify(commentsData)}' WHERE item_id=${id}`;
+      const res1 = await connection.query(updateSql);
+      ctx.body = {
+        code: 0,
+        message: '修改成功'
+      }
+    }
+  } catch (error) {
+    console.log('item222', error);
+
     ctx.body = {
       message: error.message
     };
