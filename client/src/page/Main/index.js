efe5a47cf7e72cf679c8d224ee31f5a090dd6719
AlbertBird <1533085007@qq.com>
2022-11-01 00:34:23 +0800

    fix: delete useless code

--- client/src/page/Main/index.js
+++ client/src/page/Main/index.js
@@ -1,8 +1,39 @@
 import React from 'react';
+import './index.scss';
 
 const Main = () => {
   return (
-    <div>我是Main页面</div>
+    <div className='main-container'>
+      <div className='left-container'>
+        <div className='navbar'>
+          <div className='icon'>
+          </div>
+          <div className='text'>Home</div>
+        </div>
+        <div className='navbar'>
+          <div className='icon'>
+          </div>
+          <div className='text'>My Items</div>
+        </div>
+        <div className='navbar'>
+          <div className='icon'>
+          </div>
+          <div className='text'>Account Setting</div>
+        </div>
+        <div className='navbar'>
+          <div className='icon'>
+          </div>
+          <div className='text'>Message</div>
+        </div>
+        <div className='navbar'>
+          <div className='icon'>
+          </div>
+          <div className='text'>Data</div>
+        </div>
+      </div>
+      <div className='right-container'>
+      </div>
+    </div>
   )
 }
 
